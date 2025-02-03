911b1f3dca333bd931bf858941ccc9e4dcbb5440
Kyle <kaperont@gmail.com>
2025-02-03 00:31:38 -0500

    Added MongoDB docker-compose with instructions

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 ## Development
 
+Open a terminal:
 `poetry install --with dev`
+`docker-compose up`
 
+Open a new terminal:
 `fastapi dev core/main.py`
+
+To kill the MongoDB Container:
+`docker-compose down`
--- core/settings.py
+++ core/settings.py
@@ -2,13 +2,13 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    CSRF_ALLOWED_ORIGINS: list[str] = ['http://localhost:5173']
-    JWT_SECRET: str = 'fkjahekjkfhekafjlekajkfhioej2nf982hhf3whjlH@#j32l;rj'
+    CSRF_ALLOWED_ORIGINS: list[str] = ['*']
+    JWT_SECRET: str = 'jfklajelj;kfjeklj298uf29p23u[jfo32fi2ffa;lksf]'
     JWT_ALGORITHM: str = 'HS256'
     JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-    MONGODB_URL: str = 'mongodb://localhost:27017'
-    MONGODB_READ_URL: str = 'mongodb://localhost:27017'
+    MONGODB_URL: str = 'mongodb://root:password@localhost:27017'
+    MONGODB_READ_URL: str = 'mongodb://root:password@localhost:27017'
     MONGODB_DATABASE: str = 'vst_realm'
 
 
